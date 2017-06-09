b154702c311d5fa70c3941f14b6260bb52986dea
Ivantsov Alexey <a.ivantsov@qiwi.com>
2017-06-09 17:54:23 +0300

    Add explicit timeout values for actors calling

--- src/main/scala/actors/typed/MyTyped.scala
+++ src/main/scala/actors/typed/MyTyped.scala
@@ -14,8 +14,11 @@ object MyTyped {
   sealed trait TypedMessage[+M]
 
   object TypeMessage {
+
     case class SimpleMessage[M](message: M) extends TypedMessage[M]
+
     case class MessageToRespond[M](message: M) extends TypedMessage[M]
+
   }
 
   trait TypedActor[A[+ _]] extends Actor {
@@ -37,21 +40,19 @@ object MyTyped {
 
     import scala.concurrent.ExecutionContext.Implicits.global
 
-    implicit val timeout = Timeout(10.seconds)
-
     /** it's used in case Command[Future[Result]] ===> Future[Result] **/
-    def call[B](c1: A[Future[B]])(implicit ev: ClassTag[Future[B]], ev4: ClassTag[Int]): Future[B] = {
+    def call[B](c1: A[Future[B]])(implicit ev: ClassTag[Future[B]], ev4: ClassTag[Int], timeout: Timeout): Future[B] = {
       import akka.pattern._
       (ar ? MessageToRespond(c1)).mapTo[Future[B]].flatMap(identity)
     }
 
     /** it's used in case Command[Result] ===> Future[Result] **/
-    def call[B: ClassTag](c: A[B]): Future[B] = {
+    def call[B: ClassTag](c: A[B])(implicit timeout: Timeout): Future[B] = {
       import akka.pattern.ask
       (ar ? MessageToRespond(c)).mapTo[B]
     }
 
-    /** convenient method for chaining methods**/
+    /** convenient method for chaining methods **/
     def send[B: ClassTag](c: A[B]): ActorRefTyped[A] = {
       ar ! SimpleMessage(c)
       this
--- src/main/scala/example/Example.scala
+++ src/main/scala/example/Example.scala
@@ -3,10 +3,12 @@ package example
 import actors.typed.MyTyped
 import actors.typed.MyTyped.TypedActor
 import akka.actor.ActorSystem
+import akka.util.Timeout
 import example.Example.Command.{Add, Mult}
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationDouble
 
 object Example extends App {
 
@@ -24,6 +26,7 @@ object Example extends App {
     override def call[B](c: Command[B]): B = c match {
       case Add(x) =>
         acc = acc + x
+        Thread.sleep(100)
         acc
       case Mult(x) =>
         acc = acc * x
@@ -35,6 +38,7 @@ object Example extends App {
   val actorSystem = ActorSystem()
   val calculatorActor = MyTyped.createTypedActor[Command, TypedActorImpl](actorSystem)
   val calculatorActor2 = MyTyped.createTypedActor[Command, TypedActorImpl](actorSystem)
+  implicit val timeout: Timeout = Timeout(100.nanoseconds)
 
   val sumResult = for {
     a <- calculatorActor
